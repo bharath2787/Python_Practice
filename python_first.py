84db31af2001ace48c7e10c315973086cac3af8f
bharath2787 <bharath2787@gmail.com>
2024-06-17 12:20:09 -0400

    plots

--- python_first.py
+++ python_first.py
@@ -1,2 +1,14 @@
 
-print ("hellp")
+
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+
+# Load the iris dataset
+iris = load_iris()
+
+# Plot the data
+plt.scatter(iris.data[:, 0], iris.data[:, 1], c=iris.target)
+plt.xlabel('Sepal length')
+plt.ylabel('Sepal width')
+plt.title('Iris Dataset')
+plt.show()
